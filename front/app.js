5b35b5a6ba222ab0b8add92394eba33d20de7f85
Vanessa Montoya <Vanessa@C4Qs-MacBook-Pro-4.local>
2017-01-31 14:44:42 -0500

    added app component

---
+++ front/app.js
@@ -0,0 +1,26 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var $ = require('jquery');
+var OneListing = require('./OneListing.jsx');
+var Home = require('./home.jsx');
+import {withRouter, Router, Route, Link, browserHistory} from 'react-router';
+
+var App = withRouter(React.createClass({
+  render: function() {
+    return(
+      <div>
+      <h1>ArtGal</h1>
+        <Home />
+        {this.props.children}
+      </div>
+    )
+  }
+}))
+
+ReactDOM.render(
+  <Router history={browserHistory}>
+    <Route path='/' component={App}></Route>
+    <Route path='OneListing' component={OneListing}></Route>
+  </Router>,
+  document.getElementById('root')
+)
