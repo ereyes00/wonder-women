bcf85c2396d735671558aaf84c4f812c0eed0908
sfu <sfu.concepts@gmail.com>
2017-02-01 17:59:41 -0500

    home edit

--- front/home.jsx
+++ front/home.jsx
@@ -7,30 +7,6 @@ import CarouselHome from './carousel.jsx'
 // import {baseUrl} from './config'
 
 var Home = React.createClass({
-// <<<<<<< HEAD
-// 	// componentDidMount: function(){
-// 	//     $(document).ready(function(){
-// 	//       $('.multiple-items').slick({
-// 	//           infinite: true,
-// 	//  		  slidesToShow: 3,
-// 	//   		  slidesToScroll: 3
-// 	//       });
-// 	//     });
-// 	// },
-// 	render: function(){
-// 		return(
-// 			<div className="single-item">
-			
-// 				<div><img src="//lh5.googleusercontent.com/-cTEgPOnd3l8/U8-EmaZ4KNI/AAAAAAAABc8/6eacbALkQ6A/w1358-h905-no/carousel-1.JPG" alt="" /></div>
-// 				<div><img src="//lh4.googleusercontent.com/-ntVHbbWX5eo/U8-EmV8P4cI/AAAAAAAABc4/ICYBGkcztTc/w1358-h905-no/carousel-2.jpg" alt="" /></div>
-// 				<div><img src="//lh5.googleusercontent.com/-batEXUZE_e4/U8-EmLF9-hI/AAAAAAAABc0/J3tJVUa6Buk/w1358-h905-no/carousel-3.jpg" alt="" /></div>
-// 				<div><img src="//lh5.googleusercontent.com/-gywqIeMvel0/U8-EolKdtkI/AAAAAAAABdM/G0-NHuvvJUU/w1358-h905-no/carousel-4.jpg" alt="" /></div>
-// 				<div><img src="//lh5.googleusercontent.com/--2iANjL3ikc/U8-EoGJ18mI/AAAAAAAABdI/fBe-q3Gos6Y/w1358-h905-no/carousel-5.jpg" alt="" /></div>
-// 			</div>
-
-// 		)
-// 	}
-// =======
   render: function() {
     return (
     	<div >
