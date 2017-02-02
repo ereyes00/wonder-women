213cdb5252285719a323a41af2c734470d0cc48e
sfu <sfu.concepts@gmail.com>
2017-02-02 15:23:52 -0500

    add login.jsx, signUp.jsx, css

--- front/app.jsx
+++ front/app.jsx
@@ -4,6 +4,8 @@ import Event from './event.jsx'
 import ListEvents from './listEvents.jsx';
 import Home from './home.jsx';
 import CreateEvent from './createEvent.jsx';
+import Login from './login.jsx';
+import Signup from './signUp.jsx';
 import {withRouter, Router, Route, Link, browserHistory} from 'react-router';
 //import './style/home.css';
 
@@ -22,9 +24,11 @@ var App = withRouter(React.createClass({
 ReactDOM.render(
   <Router history={browserHistory}>
     <Route path='/' component={App}></Route>
+    <Route path='login' component={Login}></Route>
+    <Route path='signup' component={Signup}></Route>
     <Route path='createevent' component={CreateEvent}></Route>
     <Route path='events/:id' component={Event}></Route>
     <Route path='events' component={ListEvents}></Route>
   </Router>,
   document.getElementById('root')
-)
+) 
