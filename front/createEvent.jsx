b4f48e4221a0e29668f8236951b049ca22d37123
sfu <sfu.concepts@gmail.com>
2017-02-02 11:46:03 -0500

    add event hours

--- front/createEvent.jsx
+++ front/createEvent.jsx
@@ -3,7 +3,7 @@ import $ from 'jquery';
 
 var CreateEvent = React.createClass({
 	getInitialState: function(){
-		return {title: '', location: '', type: '', address: '', city: '', zipCode: 0, price: '', description: '', opening: '', closing: ''}
+		return {title: '', location: '', type: '', address: '', city: '', zipCode: 0, price: '', description: '', opening: '', closing: '', hours: []}
 	},
 	// addEvent: function(){
 	// 	e.preventDefault()
@@ -37,6 +37,10 @@ var CreateEvent = React.createClass({
 	updateType: function(event){
 		this.setState({type: event.target.value})
 	},
+	updateHours: function(event){
+		this.setState({hours: event.target.value})
+		console.log('HOURS', this.state.hours)
+	},
 	render: function(){
 		return(
 			<div>
@@ -44,13 +48,17 @@ var CreateEvent = React.createClass({
 
 				<form onSubmit={this.addEvent}>
 					Event Title:
+					<br />
 					<input type="text"
 					onChange={this.handleChange.bind(this, "title")}/>
+
 					<br /><br />
 
 					Event Location:
+					<br />
 					<input type="text"
 					onChange={this.handleChange.bind(this, "location")}/>
+
 					<br /><br />
 
 					<select value={this.state.type} onChange={this.updateType}>
@@ -58,93 +66,136 @@ var CreateEvent = React.createClass({
 						<option value="Gallery">Gallery</option>
 						<option value="School">School</option>
 					</select>
+
 					<br /><br />
 
 					Address:
+					<br />
 					<input type="text"
 					onChange={this.handleChange.bind(this, "address")}/>
+
 					<br /><br />
 
 					City:
+					<br />
 					<input type="text"
 					onChange={this.handleChange.bind(this, "city")}/>
+
 					<br /><br />
 
 					Zip Code:
+					<br />
 					<input type="text"
 					onChange={this.handleChange.bind(this, "zipCode")} />
+
 					<br /><br />
 
 					Price:
+					<br />
 					<input type="text"
 					onChange={this.handleChange.bind(this, "price")}/>
+
 					<br /><br />
 
 					Description:
+					<br />
 					<input type="text"
 					onChange={this.handleChange.bind(this, "description")} />
+
 					<br /><br />
 
 					Opening Day:
+					<br />
 					<input type="date"
 					onChange={this.handleChange.bind(this, "opening")} />
+
 					<br /><br />
 
 					Closing Day:
+					<br />
 					<input type="date"
 					onChange={this.handleChange.bind(this, "closing")} />
-					<br />
-					<br />
-					Please check off the days you'll be open: 
-					<br />
-						<input type="checkbox"
-						value="Monday" />Monday
-						<br />
-						Hours:
-						<input type="text" /> to <input type="text"/>
+
+					<br /><br />
+
+
+					<strong>Please check off the days you'll be open: </strong>
+
+					<br /> <br />
+
+					<div value={this.state.hours}>
+
+						<div onChange={this.updateHours}>
+							<input type="checkbox"
+							value="Monday" />Monday
+							<br />
+							Hours:
+							<input type="text" /> to <input type="text"/>
+						</div>	
+
 						<br /><br />
 						
-						<input type="checkbox"
-						value="Tuesday" />Tuesday
-						<br />
-						Hours:
-						<input type="text" /> to <input type="text"/>
+						
+						<div onChange={this.updateHours}>
+							<input type="checkbox"
+							value="Tuesday" />Tuesday
+							<br />
+							Hours:
+							<input type="text" /> to <input type="text"/>
+						</div>
+
 						<br /><br />
 
-						<input type="checkbox"
-						value="Wednesday" />Wednesday
-						<br />
-						Hours:
-						<input type="text" /> to <input type="text"/>
+						<div onChange={this.updateHours}>
+							<input type="checkbox"
+							value="Wednesday" />Wednesday
+							<br />
+							Hours:
+							<input type="text" /> to <input type="text"/>
+						</div>
+
 						<br /><br />
 
-						<input type="checkbox"
-						value="Thursday" />Thursday
-						<br />
-						Hours:
-						<input type="text" /> to <input type="text"/>
+						<div onChange={this.updateHours}>
+							<input type="checkbox"
+							value="Thursday" />Thursday
+							<br />
+							Hours:
+							<input type="text" /> to <input type="text"/>
+						</div>
+
 						<br /><br />
 
-						<input type="checkbox"
-						value="Friday" />Friday
-						<br />
-						Hours:
-						<input type="text" /> to <input type="text"/>
+						<div onChange={this.updateHours}>
+							<input type="checkbox"
+							value="Friday" />Friday
+							<br />
+							Hours:
+							<input type="text" /> to <input type="text"/>
+						</div>
+
 						<br /><br />
 
-						<input type="checkbox"
-						value="Saturday" />Saturday
-						<br />
-						Hours:
-						<input type="text" /> to <input type="text"/>
+						<div onChange={this.updateHours}>
+							<input type="checkbox"
+							value="Saturday" />Saturday
+							<br />
+							Hours:
+							<input type="text" /> to <input type="text"/>
+						</div>
+
 						<br /><br />
 
-						<input type="checkbox"
-						value="Sunday" />Sunday
-						<br />
-						Hours:
-						<input type="text" /> to <input type="text"/>
+						<div onChange={this.updateHours}>
+							<input type="checkbox"
+							value="Sunday" />Sunday
+							<br />
+							Hours:
+							<input type="text" /> to <input type="text"/>
+						</div>
 						<br /><br />
+
+					</div>
 
 						<input type="submit" value="Submit" />
 
