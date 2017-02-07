8101ec756b50716ab233f2b0004d9a95a1126de0
sfu <sfu.concepts@gmail.com>
2017-02-07 12:59:10 -0500

    edit list of events

--- front/listEvents.jsx
+++ front/listEvents.jsx
@@ -1,41 +1,45 @@
-var React = require('react');
-import $ from 'jquery';
+import React from 'react';
 import {Link} from 'react-router';
-var Event = require('./event.jsx');
+import $ from 'jquery';
+import Event from './event.jsx';
+import Carousel from 'nuka-carousel';
 import './style/listEvents.css';
 
-var ListEvents = React.createClass({
-  getInitialState: function() {
-    return ({events: []})
+const WeekCarousel = React.createClass({
+  getInitialState: function(){
+    return{today: []}
+    // , tomorrow: [], dayAfterTomorrow: [], weekend: []
   },
-  componentDidMount: function() {
+  componentDidMount: function(){
     $.ajax({
       url: '/api/events',
       type: 'GET'
     })
     .done((data) => {
-      this.setState({events: data});
+      console.log(data)
+      var today = new Date(Date());
+      if(data.opening === today.toISOString().split('T')[0]){
+        this.setState({today: today})
+      }
     })
   },
-  ///onClick event for button ajax call to save to user's bookmarks here
-  render: function() {
-  if(this.state.events) {
+  render: function(){
+    if (this.state.events){
       return(
-        <center><div>
-        <h1>Events</h1>
-        <ul >
-          {this.state.events.map(function(event){
-            return (
-              <p key={event.id} className="event">
-              <Link to={'/events/' + event.id}>{event.title}</Link>
-              <br />
-              <br />
-              
-              </p>
-
-              )})}
-          </ul>
-        </div></center>
+        <div className="week">
+          <div className = "today">
+            <h2>Opening Today:</h2>
+            <Carousel slideWidth={0.20} cellSpacing={10}>
+              {this.state.events.map(function(event){
+                return(
+                  <Link to = {'/events/' + event.id}>
+                    <img key= {event.id} src={this.state.image} />
+                  </Link>
+                )
+              })}
+            </Carousel>
+          </div>
+        </div>
       )
     } else {
       return (<div>Loading...</div>)
@@ -43,4 +47,45 @@ var ListEvents = React.createClass({
   }
 })
 
-module.exports = ListEvents;
+export default WeekCarousel;
+
+// var ListEvents = React.createClass({
+//   getInitialState: function() {
+//     return ({events: []})
+//   },
+//   componentDidMount: function() {
+//     $.ajax({
+//       url: '/api/events',
+//       type: 'GET'
+//     })
+//     .done((data) => {
+//       this.setState({events: data});
+//     })
+//   },
+//   ///onClick event for button ajax call to save to user's bookmarks here
+//   render: function() {
+//   if(this.state.events) {
+//       return(
+//         <center><div>
+//         <h1>Opening Nights</h1>
+//         <ul >
+//           {this.state.events.map(function(event){
+//             return (
+//               <p key={event.id} className="event">
+//               <Link to={'/events/' + event.id}>{event.title}</Link>
+//               <br />
+//               <br />
+              
+//               </p>
+
+//               )})}
+//           </ul>
+//         </div></center>
+//       )
+//     } else {
+//       return (<div>Loading...</div>)
+//     }
+//   }
+// })
+
+// module.exports = ListEvents;
