edcdf106bc39dc1845a99b2a5571707f21183050
sfu <sfu.concepts@gmail.com>
2017-02-08 17:31:37 -0500

    remove todayCarousel, add ul

--- front/listEvents.jsx
+++ front/listEvents.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {Link, browserHistory} from 'react-router';
 import $ from 'jquery';
 import Event from './event.jsx';
-import Carousel from 'nuka-carousel';
 import './style/listEvents.css';
 
 const TodayCarousel = React.createClass({
@@ -16,30 +15,28 @@ const TodayCarousel = React.createClass({
       url: '/api/events/date/opening',
       type: 'GET',
       success: function(data) {
-        console.log('data', data)
         that.setState({today: data})
       }
     })  
   },
-  handleClick: function(){
-    browserHistory.push('/events/' + this.state.today.id)
-  },
+  // handleClick: function(){
+  //   browserHistory.push('/events/' + this.state.today.id)
+  // },
   render: function(){
-    console.log(this.state)
     if (this.state.today){
-      var images = []
-      this.state.today.forEach((val) => {                
-        images.push( 
-          <img key={val.id} src={val.Images[0].url} onClick={this.handleClick}/>
-        )
-      })
       return(
         <div className="week">
           <div className = "today">
-            <h2>Opening Today:</h2>
-            <Carousel slideWidth={0.20} cellSpacing={10}>
-             {images}
-            </Carousel>
+            <h2>Opening Today, {Date()}:</h2>
+             <ul>
+                {this.state.today.map((val) => {
+                  return (
+                    <p>
+                      <img className="dayEvent" key={val.id} src={val.Images[0].url} />
+                    </p>
+                  )
+                })}
+             </ul>
           </div>
         </div>
       )
