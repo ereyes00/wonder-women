00410ae56d5de6dc349867d8b653f22bf2c76b86
Vanessa Montoya <vanessa_montoya@live.com>
2017-02-22 16:49:58 -0500

    account

--- front/account.jsx
+++ front/account.jsx
@@ -50,13 +50,10 @@ const Account = React.createClass({
   },
   render: function () {
     return (
-      <div className="acctDiv">
-        <div className="welcomeBack">
-          <div className="title">
-            <h2>Welcome back, {this.state.firstName ? this.state.firstName : null}</h2>
-          </div>
+      <div className="welcomeBack">
 
-          <div className="">
+            <h2>Welcome back, {this.state.firstName ? this.state.firstName : null}</h2>
+
             <h3 className="text">Your Account Info:</h3>
             <p className="text"> <b>Name: </b> 
               {this.state.firstName}{' '}{this.state.lastName}
@@ -64,13 +61,12 @@ const Account = React.createClass({
             <p className="text"><b>Email: </b> 
               {this.state.email}
             </p>
-          </div>
-          <hr />
+
+            <hr />
           {
           //<UserBookmarks />
           }
-          <div className="">
-            <h3 className="text">Your Bookmarked Events:</h3>
+            <h3 className="text">Your Bookmarked Events: </h3>
             <ul>
               {!this.state.bookmarks ? "You have not Bookmarked any events." : this.state.bookmarks.map((val, idx) => {
                 return(
@@ -78,42 +74,22 @@ const Account = React.createClass({
                   </Link>)
               })}
             </ul>
-          </div>
-          
-          {
-          // <div className="">
-          //   <h3>Your Created Events:</h3>
-          //   <ul className="listStyle">
-          //     {!this.state.createdEvents ? "You have not created any events." : this.state.createdEvents.map((val, idx) => {
-          //       return(
-          //         <Link to={'/events/' + val.id}><li key={idx} className="listStyle"><img src={val.Images[0].url} width="205" height="auto"></img><div className="imageTitle">{val.title}</div></li>
-          //         </Link>)
-          //     })}
-          //   </ul>
-          // </div>
-          }
 
-          <h3>Your Created Events:</h3>
+          <h3>Your Created Events: </h3>
           <div className="exhibitions-list">
               {!this.state.createdEvents ? "You have not created any events." : this.state.createdEvents.map((val, idx) => {
                 return(
-                  <Link to={'/events/' + val.id} className="box"><span key={idx} className="box-inside"><img src={val.Images[0].url} width="205" height="auto" className="images"></img><span className="box-text"><p className="box-title">{val.title}</p></span></span>
+                  <Link to={'/events/' + val.id} ><span key={idx} className="box"><span className="box-text">{val.title}</span><span className="image-container"><img src={val.Images[0].url} className="images"></img></span></span>
                   </Link>)
               })}
           </div>
-
-        </div>
-            <br /><br />
-
-        <div className="acctButtons">
-          <Link to={'/createlocation/'}><button className='button'>Create A Location</button>
-          </Link>
-          <br />
-          <Link to={'/createevent/'}><button className='button'>Create An Event</button>
-          </Link>
-          <br />
-          <br />
-        </div>
+            <div className="acctButtons">
+              <Link to={'/createlocation/'}><button className='button'>Create A Location</button>
+              </Link>
+              <br />
+              <Link to={'/createevent/'}><button className='button'>Create An Event</button>
+              </Link>
+            </div>
       </div>
     );
   }
